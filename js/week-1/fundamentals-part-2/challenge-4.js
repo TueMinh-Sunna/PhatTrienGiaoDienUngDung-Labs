8df7febb32f66d517e483f25012f278497d517e2
TueMinh-Sunna <23630701.minh@student.iuh.edu.vn>
2026-01-12 17:10:18 +0700

    change folder names

---
+++ js/week-1/fundamentals-part-2/challenge-4.js
@@ -0,0 +1,18 @@
+let bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52]
+let tips = []
+let totals = []
+
+function calcTip (bill) {
+    return bill < 50 ? 0 : bill <= 300 ? .15*bill : .2*bill
+}
+
+for (bill of bills) {
+    let tip = calcTip(bill)
+    tips.push(tip)
+    let total = bill + tip
+    totals.push(total)
+}
+
+function calcAverage (array) {
+    
+}
