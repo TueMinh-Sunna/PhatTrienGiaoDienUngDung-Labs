365539bb718e6f7be3b710aab4600d30e85173b4
TueMinh-Sunna <23630701.minh@student.iuh.edu.vn>
2026-01-14 16:48:15 +0700

    add w1p27c1

---
+++ js/week1/p27-oop/challenge1.js
@@ -0,0 +1,19 @@
+function Car (make, speed) {
+    this.make = make
+    this.speed = speed
+}
+
+Car.prototype.accelerate = function() {
+    this.speed += 10
+    console.log(this.speed);
+}
+
+Car.prototype.brake = function () {
+    this.speed -= 5
+    console.log(this.speed);
+}
+
+car1 = new Car('BMW',120)
+car1.accelerate()
+car1.accelerate()
+car1.brake()
