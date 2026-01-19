1d13cfe9f126df0dddbe4a6c43a971c8b889c818
H82M19\Student <Student>
2026-01-19 08:59:53 +0700

    add reactw1l1

---
+++ reactjs/week1/src/components/ProductCard.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import './ProductCard.css'
+
+const ProductCard = ({src, name, price}) => {
+    return (
+        <div className="product-card">
+            <img src={src} alt={name} className="product-image"/>
+            <h3 className="product-name">{name}</h3>
+            <p className="product-price">{price}</p>
+            <button className="add-to-cart">Add to cart</button>
+        </div>
+    )
+}
+
+export default ProductCard
